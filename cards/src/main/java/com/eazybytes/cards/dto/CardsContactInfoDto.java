eac5116d9c01e64c1d5eacdad5a6caa89cd4b172
randrin <nzeukangrandrin@gmail.com>
2025-02-11 14:44:59 +0100

    Revert "Set up configuration"
    This reverts commit 553b6f753d0abfd1de08ed7907848d2ddc811c72.

--- cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
@@ -1,19 +1,10 @@
 package com.eazybytes.cards.dto;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-@Getter
-@Setter
-public class CardsContactInfoDto {
-
-    private String message;
-    private Map<String, String> contactDetails;
-    private List<String> onCallSupport;
+public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
 }
